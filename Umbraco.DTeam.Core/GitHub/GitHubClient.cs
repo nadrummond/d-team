f773532585c287ec3579b09b4d8ed98b3f2b40c6
Shannon <sdeminick@gmail.com>
2017-12-22 11:13:32 +1100

    adds missing GitHubClient

---
+++ Umbraco.DTeam.Core/GitHub/GitHubClient.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Umbraco.Core.Logging;
+
+namespace Umbraco.DTeam.Core.GitHub
+{
+    public class GitHubClient
+    {
+        private readonly HttpClient _client;
+
+        public GitHubClient()
+        {
+            _client = new HttpClient(new HttpClientHandler
+            {
+                UseProxy = true
+            })
+            {
+                BaseAddress = new Uri("https://api.github.com/")
+            };
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            _client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Mozilla", "5.0"));
+        }
+
+        public GithubSearchResult GetNumberOfOpenPullRequests()
+        {
+            var openPullRequestUrl = "search/issues?q=is:pr+repo:umbraco/umbraco-cms+state:open";
+            var githubSearchResult = new GithubSearchResult()
+            {
+                TotalCount = -1
+            };
+            try
+            {
+                githubSearchResult = JsonConvert.DeserializeObject<GithubSearchResult>(_client.GetAsync(openPullRequestUrl).Result.Content.ReadAsStringAsync().Result);
+            }
+            catch (Exception ex)
+            {
+                LogHelper.WarnWithException<GitHubClient>("Error while fetching PR count from Github via url {0}", ex, (Func<object>) (() => openPullRequestUrl));
+            }
+            return githubSearchResult;
+        }
+    }
+}
---
+++ Umbraco.DTeam.Core/GitHub/GitHubSearchResult.cs
@@ -0,0 +1,16 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Umbraco.DTeam.Core.GitHub.GithubSearchResult
+// Assembly: Umbraco.DTeam.Core, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 9C2125CE-8AAC-4B0A-9E34-E96211E4B131
+// Assembly location: C:\Users\Shannon\Downloads\Umbraco.DTeam.Core.dll
+
+using Newtonsoft.Json;
+
+namespace Umbraco.DTeam.Core.GitHub
+{
+    public class GithubSearchResult
+    {
+        [JsonProperty("total_count")]
+        public int TotalCount { get; set; }
+    }
+}
