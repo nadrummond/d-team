8e96a581c59ae3a1f72522b8011a097e91c49a1b
Stephan <stephane@umbraco.dk>
2018-02-25 17:02:01 +0100

    Fix GitHub, VSTS

--- Umbraco.DTeam.Core/GitHub/GitHubClient.cs
+++ Umbraco.DTeam.Core/GitHub/GitHubClient.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Umbraco.Core.Logging;
@@ -21,6 +22,11 @@ namespace Umbraco.DTeam.Core.GitHub
             };
             _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             _client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Mozilla", "5.0"));
+
+            // read https://stackoverflow.com/questions/2859790/the-request-was-aborted-could-not-create-ssl-tls-secure-channel
+            // this is required to talk to GitHub
+            // but... it's a global setting, what are the consequences?
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
         }
 
         private GithubSearchResult _githubSearchResult;
@@ -33,20 +39,15 @@ namespace Umbraco.DTeam.Core.GitHub
                 if (_available.HasValue) return _available.Value;
 
                 var openPullRequestUrl = "search/issues?q=is:pr+repo:umbraco/umbraco-cms+state:open";
-                var githubSearchResult = new GithubSearchResult()
-                {
-                    TotalCount = -1
-                };
                 try
                 {
-                    
                     _githubSearchResult = JsonConvert.DeserializeObject<GithubSearchResult>(_client.GetAsync(openPullRequestUrl).Result.Content.ReadAsStringAsync().Result);
                     _available = true;
                 }
                 catch (Exception ex)
                 {
                     LogHelper.WarnWithException<GitHubClient>("Error while fetching PR count from Github via url {0}", ex, (Func<object>) (() => openPullRequestUrl));
-                    _available = true;
+                    _available = false;
                 }
                 return _available.Value;
             }
--- Umbraco.DTeam.Core/Vsts/VstsClient.cs
+++ Umbraco.DTeam.Core/Vsts/VstsClient.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using Newtonsoft.Json;
+using Umbraco.Core;
 using Umbraco.Core.IO;
+using Umbraco.Core.Logging;
 
 namespace Umbraco.DTeam.Core.Vsts
 {
@@ -18,6 +21,16 @@ namespace Umbraco.DTeam.Core.Vsts
         // version 3.0 is the current latest
         // https://www.visualstudio.com/en-us/docs/integrate/get-started/rest/basics
         private const string ApiVersion = "3.0";
+        private const int Top = 32;
+
+        // this should come from the CMS really
+        private static readonly string[] Projects = {
+            "Umbraco Cms",
+            "Umbraco Deploy",
+            "Umbraco Deploy Contrib",
+            "Umbraco Forms",
+            "Umbraco Duck",
+        };
 
         private readonly HttpClient _httpClient;
         private string _token;
@@ -48,17 +61,17 @@ namespace Umbraco.DTeam.Core.Vsts
             }
         }
 
-        public BuildsModel TheBuildsModel
+        public BuildsModel[] BuildsModels
         {
             get
             {
                 if (!_available.HasValue)
                     _available = IsAvailable;
-                return _buildsModel;
+                return _buildsModels;
             }
         }
 
-        private BuildsModel _buildsModel;
+        private BuildsModel[] _buildsModels;
         private bool? _available;
 
         public bool IsAvailable
@@ -68,34 +81,49 @@ namespace Umbraco.DTeam.Core.Vsts
                 if (_available.HasValue) return _available.Value;
                 try
                 {
-                    //var url = $"https://umbraco.VisualStudio.com/DefaultCollection/_apis/projects/e31cd6bd-f85b-4b61-81d9-eb7fa77eec9e?api-version={ApiVersion}";
-                    var url = $"https://umbraco.VisualStudio.com/DefaultCollection/e31cd6bd-f85b-4b61-81d9-eb7fa77eec9e/_apis/build/builds?api-version={ApiVersion}&$top=20";
-                    var request = new HttpRequestMessage(HttpMethod.Get, url);
-                    var mediaType = new MediaTypeWithQualityHeaderValue("application/json");
-                    request.Headers.Accept.Add(mediaType);
-                    var auth = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes(":" + Token)));
-                    request.Headers.Authorization = auth;
-                    var response = _httpClient.SendAsync(request).Result;
-                    response.EnsureSuccessStatusCode();
-                    var json = response.Content.ReadAsStringAsync().Result;
-                    _buildsModel = JsonConvert.DeserializeObject<BuildsModel>(json); // fixme or directly read as ... ?!
+                    _buildsModels = Projects.Select(GetProject).ToArray();
                     _available = true;
-                    return true;
                 }
-                catch
+                catch (Exception ex)
                 {
+                    LogHelper.WarnWithException<VstsClient>("Failed to talk to VSTS.", ex);
                     _available = false;
-                    return false;
                 }
+                return _available.Value;
             }
+        }
+
+        private BuildsModel GetProject(string name)
+        {
+            //var url = $"https://umbraco.VisualStudio.com/DefaultCollection/_apis/projects/e31cd6bd-f85b-4b61-81d9-eb7fa77eec9e?api-version={ApiVersion}";
+            var url = $"https://umbraco.VisualStudio.com/DefaultCollection/{name}/_apis/build/builds?api-version={ApiVersion}&$top={Top}";
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var mediaType = new MediaTypeWithQualityHeaderValue("application/json");
+            request.Headers.Accept.Add(mediaType);
+            var auth = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes(":" + Token)));
+            request.Headers.Authorization = auth;
+            var response = _httpClient.SendAsync(request).Result;
+            response.EnsureSuccessStatusCode();
+            var json = response.Content.ReadAsStringAsync().Result;
+            var buildsModel = JsonConvert.DeserializeObject<BuildsModel>(json); // fixme or directly read as ... ?!
+            buildsModel.Project = name;
+            return buildsModel;
         }
 
         public class BuildsModel
         {
+            public string Project { get; set; }
+
             public int Count { get; set; }
 
             [JsonProperty("value")]
             public List<Build> Builds { get; set; }
+
+            [JsonIgnore]
+            public IEnumerable<Build> FilteredBuilds => Builds
+                .DistinctBy(x => x.SourceBranch.TrimStart("refs/heads/"))
+                .Where(x => !x.SourceBranch.StartsWith("refs/pull/"))
+                .OrderBy(x => x.SourceBranch);
         }
 
         public class Build
